59257081d4ef8bcf2264c89ffdfc309b9774d119
Samael Medina <samaelortiz2218@gmail.com>
2026-08-15 14:43:24 -0700

    feat: scaffold SupportUnit model, controller, requests, migration, and seeder

---
+++ app/Http/Controllers/SupportUnitController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSupportUnitRequest;
+use App\Http\Requests\UpdateSupportUnitRequest;
+use App\Models\SupportUnit;
+
+class SupportUnitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSupportUnitRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SupportUnit $supportUnit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SupportUnit $supportUnit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSupportUnitRequest $request, SupportUnit $supportUnit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SupportUnit $supportUnit)
+    {
+        //
+    }
+}
