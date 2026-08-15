d573d5a3b7f4fd74634b96bc590b6dfc7727554e
Samael Medina <samaelortiz2218@gmail.com>
2026-08-15 14:54:30 -0700

    feat: initialize ticket system models, controllers, requests, and migrations while making user support unit relation nullable

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreignId('support_unit_id')->constrained('support_units')->cascadeOnDelete();
+            $table->foreignId('support_unit_id')->nullable()->constrained('support_units')->cascadeOnDelete();
             $table->rememberToken();
             $table->timestamps();
         });
